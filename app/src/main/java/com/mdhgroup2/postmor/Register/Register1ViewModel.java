66906e115d2a5c4e5ee7d9e8713ee5cffe61245c
Philip Karlsson <karlsson914@gmail.com>
2019-12-16 15:21:05 +0100

    Adding string resources and simple layouts
    - Designed simple layouts for Register and Sign In
    
    TODO: 
    - Implement navigation between fragments such as home-> settings etc.
    https://developer.android.com/guide/navigation/navigation-getting-started#navigate
     - Add a ViewPager to scroll through multi page views such as Register.
    https://stackoverflow.com/questions/20586619/android-viewpager-with-bottom-dots

---
+++ app/src/main/java/com/mdhgroup2/postmor/Register/Register1ViewModel.java
@@ -0,0 +1,7 @@
+package com.mdhgroup2.postmor.Register;
+
+import androidx.lifecycle.ViewModel;
+
+public class Register1ViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/mdhgroup2/postmor/Register/Register3ViewModel.java
@@ -0,0 +1,7 @@
+package com.mdhgroup2.postmor.Register;
+
+import androidx.lifecycle.ViewModel;
+
+public class Register3ViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
