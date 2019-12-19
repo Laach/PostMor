d8adeaa1cf4d470f974f74f7621c890524ccca62
Casper Andersson <andersson.casper99@gmail.com>
2019-12-19 14:01:48 +0100

    Changed type of getPickuptime from LocalTime to String
    LocalTime was a pain to work with and some features weren't available in API v.24.

--- app/src/main/java/com/mdhgroup2/postmor/database/interfaces/ILetterManager.java
+++ app/src/main/java/com/mdhgroup2/postmor/database/interfaces/ILetterManager.java
@@ -7,7 +7,7 @@ import com.mdhgroup2.postmor.database.DTO.EditMsg;
 import java.time.LocalTime;
 
 public interface ILetterManager {
-    LocalTime getPickupTime();
+    String getPickupTime();
     LiveData<EditMsg> getOrStartDraft(int recipientID);
     LiveData<EditMsg> getOrStartGenerictDraft();
     void sendDraft(int recipientID);
