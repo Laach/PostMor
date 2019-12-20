02c0bb14fe6d6448afb7c6e030600afeba506a8d
Emil <emil.elfstrom99@gmail.com>
2019-12-20 12:49:11 +0100

    Change password now uses proper password type

--- app/src/main/java/com/mdhgroup2/postmor/Settings/SettingsFragment.java
+++ app/src/main/java/com/mdhgroup2/postmor/Settings/SettingsFragment.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.mdhgroup2.postmor.R;
 
@@ -25,15 +28,23 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
         setPreferencesFromResource(R.xml.preferences, rootKey);
-    }
-/*
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings_fragment, container, false);
+        EditTextPreference preference = findPreference("password");
+
+        if (preference!= null) {
+            preference.setOnBindEditTextListener(
+                    new EditTextPreference.OnBindEditTextListener() {
+                        @Override
+                        public void onBindEditText(@NonNull EditText editText) {
+                            editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        }
+                    });
+        }
+
     }
 
+/*
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
