b86d315ee64bad8021c4081031cd8c29dae73b4d
Philip Karlsson <karlsson914@gmail.com>
2019-12-20 11:02:38 +0100

    Fixed most of the underlying structure for compose

--- app/src/main/java/com/mdhgroup2/postmor/Compose/ComposeFragment.java
+++ app/src/main/java/com/mdhgroup2/postmor/Compose/ComposeFragment.java
@@ -7,16 +7,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.mdhgroup2.postmor.Home.HomeFragmentDirections;
 import com.mdhgroup2.postmor.R;
 
 public class ComposeFragment extends Fragment {
 
     private ComposeViewModel mViewModel;
+    NavController navController;
+
 
     public static ComposeFragment newInstance() {
         return new ComposeFragment();
@@ -32,6 +38,14 @@ public class ComposeFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ComposeViewModel.class);
+
+        View view = getView();
+        navController = Navigation.findNavController(view);
+
+        Button typeButton = view.findViewById(R.id.typeButton);
+        Button handButton = view.findViewById(R.id.handButton);
+        typeButton.setOnClickListener(Navigation.createNavigateOnClickListener(ComposeFragmentDirections.actionComposeFragmentToCompose2Typed()));
+        handButton.setOnClickListener(Navigation.createNavigateOnClickListener(ComposeFragmentDirections.actionComposeFragmentToCompose2Handwritten()));
         // TODO: Use the ViewModel
     }
 
---
+++ app/src/main/java/com/mdhgroup2/postmor/Compose/RecipientViewModel.java
@@ -0,0 +1,7 @@
+package com.mdhgroup2.postmor.Compose;
+
+import androidx.lifecycle.ViewModel;
+
+public class RecipientViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
