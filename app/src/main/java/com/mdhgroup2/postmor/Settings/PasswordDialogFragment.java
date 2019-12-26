48a1658189a95a5438548f84098ec8dfe15cc67b
Emil <emil.elfstrom99@gmail.com>
2019-12-26 13:40:09 +0100

    Added check for confirm password
    TODO: check if old password is correct

--- app/src/main/java/com/mdhgroup2/postmor/Settings/PasswordDialogFragment.java
+++ app/src/main/java/com/mdhgroup2/postmor/Settings/PasswordDialogFragment.java
@@ -5,6 +5,10 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,13 +22,19 @@ public class PasswordDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.change_password_fragment,null);
 
-        builder.setView(inflater.inflate(R.layout.change_password_fragment, null))
+        final EditText newPassword = dialogView.findViewById(R.id.newPassword);
+        final EditText confirmNewPassword = dialogView.findViewById(R.id.confirmNewPassword);
+        final TextView passwordHint = dialogView.findViewById(R.id.passwordHint);
+
+        builder.setView(dialogView)
                 .setMessage("Change Password")
                 .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         //TODO: check if old password is correct
+
                     }
                 }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
@@ -32,6 +42,27 @@ public class PasswordDialogFragment extends DialogFragment {
                 PasswordDialogFragment.this.getDialog().cancel();
             }
         });
-        return builder.create();
+
+        final AlertDialog dialog = builder.create();
+
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newP = newPassword.getText().toString();
+                String confP = confirmNewPassword.getText().toString();
+
+                if (!newP.isEmpty() || !confP.isEmpty()) {
+                    if(newP.equals(confP)){
+                        dialog.dismiss();
+                    }
+                    else{
+                        passwordHint.setText("Passwords are not the same!");
+                    }
+                }
+            }
+        });
+
+        return dialog;
     }
 }
