6ca114ae6a729ad6cd688460f4f77d608d28f121
Emil <emil.elfstrom99@gmail.com>
2019-12-27 10:50:41 +0100

    Added reyclerview to compose2

---
+++ app/src/main/java/com/mdhgroup2/postmor/Compose/Compose2HandRecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package com.mdhgroup2.postmor.Compose;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mdhgroup2.postmor.R;
+
+public class Compose2HandRecyclerViewAdapter extends RecyclerView.Adapter<Compose2HandRecyclerViewAdapter.ComposeViewHolder> {
+    private String[] mDataset = {"Image1", "Image2", "Image3"};
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ComposeViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public View imageItem;
+        public TextView text;
+        public ImageView image;
+        public ComposeViewHolder(View v) {
+            super(v);
+            imageItem = v;
+            text = imageItem.findViewById(R.id.imageItemText);
+            image = imageItem.findViewById(R.id.imageItemImage);
+
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public Compose2HandRecyclerViewAdapter() {
+
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public Compose2HandRecyclerViewAdapter.ComposeViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View v = (View) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.compose2_image_item, parent, false);
+
+        ComposeViewHolder vh = new ComposeViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ComposeViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.text.setText(mDataset[position]);
+        holder.image.setImageResource(R.mipmap.ic_launcher);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
--- app/src/main/java/com/mdhgroup2/postmor/Compose/Compose2Handwritten.java
+++ app/src/main/java/com/mdhgroup2/postmor/Compose/Compose2Handwritten.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,9 @@ import android.view.ViewGroup;
 import com.mdhgroup2.postmor.R;
 
 public class Compose2Handwritten extends Fragment {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     private Compose2HandwrittenViewModel mViewModel;
 
@@ -25,7 +30,16 @@ public class Compose2Handwritten extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.compose2_handwritten_fragment, container, false);
+        View view = inflater.inflate(R.layout.compose2_handwritten_fragment, container, false);
+
+        recyclerView = view.findViewById(R.id.compose2HandwrittenRecyclerView);
+        layoutManager = new LinearLayoutManager(container.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new Compose2HandRecyclerViewAdapter();
+        recyclerView.setAdapter(mAdapter);
+
+        return view;
     }
 
     @Override
