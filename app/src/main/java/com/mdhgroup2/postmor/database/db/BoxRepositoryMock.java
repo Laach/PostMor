f9cdbefc396d00b1189d6f9405e052ae297a1c68
Casper Andersson <andersson.casper99@gmail.com>
2020-01-06 15:42:16 +0100

    Removed LiveData from outgoingLetterCount

--- app/src/main/java/com/mdhgroup2/postmor/database/db/BoxRepositoryMock.java
+++ app/src/main/java/com/mdhgroup2/postmor/database/db/BoxRepositoryMock.java
@@ -141,13 +141,9 @@ public class BoxRepositoryMock implements IBoxRepository {
     }
 
     @Override
-    public LiveData<Integer> outgoingLetterCount() {
-        LiveData<Integer> ld =  new LiveData<Integer>() { {
-                postValue(7);
-            }
-        };
+    public int outgoingLetterCount() {
 
-        return ld;
+        return 2;
     }
 
     @Override
