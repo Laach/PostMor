0ac2551e603b276b16c838239d32521fb144e802
Philip Karlsson <karlsson914@gmail.com>
2020-01-06 14:20:54 +0100

    Styling, Loading message data, etc
    - Now loads message data in the box, both pictures and text.
    - Added some cardViews. They look more material.
    - Now profile pictures are shown as a gray anon image if not available. They are also round.
    - New and improved color sheme

--- app/src/main/java/com/mdhgroup2/postmor/database/db/BoxRepositoryMock.java
+++ app/src/main/java/com/mdhgroup2/postmor/database/db/BoxRepositoryMock.java
@@ -3,6 +3,7 @@ package com.mdhgroup2.postmor.database.db;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
 
 import androidx.lifecycle.LiveData;
 
@@ -35,13 +36,15 @@ public class BoxRepositoryMock implements IBoxRepository {
         MsgCard u1 = new MsgCard();
         u1.Name = "Ann-Marie Josefsson";
         u1.Address = "Isterbarnsgatan 12";
-        u1.Picture = null;
+        u1.Picture = BitmapFactory.decodeResource(DatabaseClient.appContext.getResources(),
+                R.drawable.stefan);
+
         u1.IsFriend = true;
         u1.DateStamp = Utils.makeDate(2019, 12, 19);
         u1.MsgID = 1;
         u1.IsSentByMe = true;
         u1.UserID = 5;
-        u1.Text = "This is a message";
+        u1.Text = "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little &quot;clever&quot; comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.";
 
         MsgCard u2 = new MsgCard();
         u2.Name = "Arne Askersund";
@@ -72,6 +75,7 @@ public class BoxRepositoryMock implements IBoxRepository {
             String data = myReader.nextLine();
 //            System.out.println(data);
              u3.Images.add(Converters.fromBase64(data));
+             u3.Images.add(u1.Picture);
         }
         myReader.close();
 
